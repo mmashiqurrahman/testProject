c4d8c477564b14552c31813afc261f5417907456
mmashiqurrahman <m.m.ashiqur.rahman@gmail.com>
2025-03-12 21:33:55 +0600

    1st commit, March 12, 2025

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -27,7 +27,14 @@
     </div>
 </body>
 <script>
-    const pusher = new Pusher("{{ config('broadcasting.connections.pusher.key') }}", {cluster: 'eu'});
+    const pusher = new Pusher("{{ config('broadcasting.connections.pusher.key') }}", {
+        wsHost: "127.0.0.1",  // Use your server's domain or IP
+        wsPort: 6001,  // Laravel WebSockets default port
+        forceTLS: false,  // Set to true if using HTTPS
+        disableStats: true,  // Avoid making requests to Pusher's stats service
+        enabledTransports: ["ws", "wss"]  // Use WebSockets only
+    });
+
     const channel = pusher.subscribe('public');
     console.log("{{ config('broadcasting.connections.pusher.key') }}");
 
